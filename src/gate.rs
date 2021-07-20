a907609cdfc6769a8010b45a2608680560120f21
Bastien Aracil <findbastien@gmail.com>
2021-07-21 00:11:05 +0200

    apply cargo fix

--- src/gate.rs
+++ src/gate.rs
@@ -1,15 +1,15 @@
-use std::ops::Sub;
 
-use num_complex::Complex64;
-use num_traits::One;
+
+
+
 use serde::{Deserialize, Serialize};
 
-use crate::gate_op::hadamard::apply_controlled_hadamard;
-use crate::gate_op::pauli::{apply_controlled_not, apply_controlled_pauli_x, apply_controlled_pauli_y, apply_controlled_pauli_z};
-use crate::gate_op::swap::apply_controlled_swap;
+
+
+
 use crate::gate_without_control::GateWithoutControl;
-use crate::operation::CircuitElement;
-use crate::state::QuantumState;
+
+
 
 #[derive(Clone, Serialize, Deserialize)]
 pub struct Gate {
